d069b56da0ee788fddf5440b5b759df5f193f0c4
Sai Kiran <Sai@example.com>
2022-07-23 21:53:11 +0530

    First commit

---
+++ DatatypeProgram.java
@@ -0,0 +1,26 @@
+package phase1.programs;
+
+public class DatatypeProgram {
+
+	public static void main(String[] args) {
+		
+		String name = "Sai kiran";
+		int age=21;
+		float f=45.6f;
+		long l=875630294l;
+		double d=2345677.89744426424741;
+		char gender = 'M';
+		boolean status = true;
+		
+		System.out.println("My name is"+" "+ name);
+		System.out.println("My char value is"+" "+ gender);
+		System.out.println("My age is"+" "+age);
+		System.out.println("My status is"+" "+ status);
+		System.out.println("My float value is"+" "+ f);
+		System.out.println("My long value is"+" "+ l);
+		System.out.println("My double value is"+" "+ d);
+		
+
+	}
+
+}
